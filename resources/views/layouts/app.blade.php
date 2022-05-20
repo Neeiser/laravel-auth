ea7c3be27590be37ea258ab6e2dcb3ff7e9bf3c9
Neeiser <stortini.study@gmail.com>
2022-05-20 18:08:08 +0200

    View.blade for all the routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,17 +55,17 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
                         @endguest
                     </ul>
                 </div>
